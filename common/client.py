da99c84f3a78a46c4b42d4ebb0995539c23c7ba4
Costas Skarakis <skarcos@gmail.com>
2018-11-11 20:42:18 +0200

    Added sip specific socket reader

--- common/client.py
+++ common/client.py
@@ -12,8 +12,10 @@ class TCPClient(object):
         self.port = port
         self.rip, self.rport = None, None
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.socket.bind((self.ip, self.port))
         self.socket.settimeout(5.0)
+        self.sockfile = self.socket.makefile(mode='rb')
 
     def connect(self, dest_ip, dest_port):
         self.socket.connect((dest_ip, dest_port))
@@ -25,12 +27,13 @@ class TCPClient(object):
         print(data)
         if type(data) == type(b''):
             self.socket.sendall(data)
-            debug("Sent:\n\n" + data.decode("utf8", "backslashreplace").replace("\r\n", "\n"))
+            debug("Sent from port {}:\n\n".format(self.port) + data.decode("utf8", "backslashreplace").replace("\r\n", "\n"))
         else:
             self.socket.sendall(bytes(data, encoding))
-            debug("Sent:\n\n" + data.replace("\r\n", "\n"))
+            debug("Sent from port {}:\n\n".format(self.port) + data.replace("\r\n", "\n"))
 
     def waitForData(self, timeout=None, buffer=4096):
+        debug("Waiting on port {}".format(self.port))
         bkp = self.socket.gettimeout()
         if timeout: self.socket.settimeout(timeout)
         try:
@@ -40,6 +43,40 @@ class TCPClient(object):
         debug("Received:\n\n" + data.decode("utf8", "backslashreplace").replace("\r\n", "\n"))
         return data
 
+    def waitForSipData(self, timeout=None, buffer=4096):
+        debug("Waiting on port {}".format(self.port))
+        bkp = self.socket.gettimeout()
+        if timeout: self.socket.settimeout(timeout)
+        try:
+            content_length = -1
+            data = self.sockfile.readline()
+
+            while True:
+                line = self.sockfile.readline()
+                data += line
+                if not line.strip():
+                    break
+                header, value = [x.strip() for x in line.split(b":", 1)]
+                if header == b"Content-Length": content_length = int(value)
+
+            if content_length > 0:
+                data += self.sockfile.read(content_length)
+
+            if content_length == -1:
+                debug(data.decode())
+                raise Exception("No content length in message")
+        except ValueError:
+            debug(data.decode())
+            debug(line.decode())
+            raise
+        except socket.timeout:
+            debug(data)
+            raise
+        finally:
+            self.socket.settimeout(bkp)
+        debug("Received:\n\n" + data.decode("utf8").replace("\r\n", "\n"))
+        return data
+
     def waitForCstaData(self):
         header = self.socket.recv(4)
         datalength = int(''.join(["%02X" % x for x in header]), base=16) - 4
@@ -47,5 +84,5 @@ class TCPClient(object):
         while len(data) < datalength:
             data += self.socket.recv(datalength - len(data))
         # print('message:\n{}\nsize{}\n'.format(data,datalength))
-        debug("Received:\n\n" + data.decode("utf8", "backslashreplace").replace("\r\n", "\n"))
+        debug("Received:\n\n" + (header + data).decode("utf8", "backslashreplace").replace("\r\n", "\n"))
         return header + data
