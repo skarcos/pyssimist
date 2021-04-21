40a5d7f09421fa4efb460ae87a121556a9542a9f
Costas Skarakis <skarcos@gmail.com>
2021-04-21 18:03:02 +0300

    First version of setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pyssimist',
+    version='0.1.0',
+    packages=['sip', 'csta', 'common', 'tshark_tools'],
+    url='https://github.com/skarcos/pyssimist',
+    license='GPL-3.0',
+    author='Costas Skarakis',
+    author_email='skarcos@gmail.com',
+    description='A Python Network Traffic Simulator... It\'s probably not going to work.'
+)
